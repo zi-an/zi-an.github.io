9ff9131e935af825fe324183439bb78a2be38fae
zian <zian@zi.an>
2023-05-04 17:27:09 +0800

    update ipv6

--- ipv6update.sh
+++ ipv6update.sh
@@ -1,7 +1,16 @@
-cd ~/ipv6 
-sh -c "docker exec nginx ip -6 addr | grep 240" > ipv6.txt
-#sleep 2s
+#!/bin/bash
 
-git add .
-git commit -m "update ipv6"
-git push -u origin ipv6
+cd ~/ipv6 
+sh -c "docker exec nginx ip -6 addr | grep 240" > now.txt
+
+dif=`diff ~/ipv6/last.txt ~/ipv6/now.txt`
+echo ${dif}
+if [ ${dif} ];then
+ echo "no change"
+else
+ rm ~/ipv6/last.txt
+ cp ~/ipv6/now.txt ~/ipv6/last.txt
+ git add .
+ git commit -m "update ipv6"
+ git push -u origin ipv6
+fi
