9cc918732d4f29b543dd13524904577942e4d886
zian <zian@zi.an>
2023-05-07 17:42:42 +0800

    update ipv6

--- ipv6update.sh
+++ ipv6update.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-cd ~/ipv6 
-sh -c "docker exec nginx ip -6 addr | grep 240" > now.txt
+cd ~/ipv6
 
-now=`md5sum now.txt`
+docker=`sh -c 'docker exec nginx ip -6 addr | grep 240'|awk '{ print $2 }'|awk -F'/64' '{ print $1 }'|awk 'NR==1'`
+now=`echo $docker|md5sum`
+
 last=`md5sum last.txt`
 if [ ${now:0:32} == ${last:0:32} ] ;then
  echo "no change"
 else
- rm last.txt
- cp now.txt last.txt
+ echo ${now} > last.txt 
  git add .
  git commit -m "update ipv6"
  git push -u origin ipv6
