bfaf373ee578b4dbacda9b5a75b8c7b95b4e5689
root <root@206.mm>
2023-08-20 07:11:51 +0800

    update ipv6

--- ipv6update.sh
+++ ipv6update.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#cd ~/ipv6
+dir=`dirname ${BASH_SOURCE}`
 
 docker=`sh -c 'ip -6 addr | grep 240'|awk '{ print $2 }'|awk -F'/64' '{ print $1 }'|awk 'NR==1'`
 now=`echo $docker|md5sum`
 
-last=`md5sum ./last.txt`
+last=`md5sum $dir/last.txt`
 if [ ${now:0:32} == ${last:0:32} ] ;then
  echo "no change"
 else
- echo -n $docker > ./last.txt 
+ echo -n $docker > $dir/last.txt 
  git add .
  git commit -m "update ipv6"
  git push -u origin ipv6
