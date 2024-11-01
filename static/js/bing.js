21db8e34fa06f59c55677d5d284a42fbd05a9a20
zian <zian@zi.an>
2024-11-02 00:24:05 +0800

    Signed-off-by: zian <zian@zi.an>

---
+++ static/js/bing.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         bing
+// @version      2024-10-12
+// @description  必应搜索屏蔽知乎;https://cn.bing.com/search?q=-site%3Azhihu.com%20
+// @match        https://cn.bing.com/*search?*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    //忽略zhihu.com
+    var filter='-site:zhihu.com'
+    var url=window.location.href
+    if(url.indexOf('-site')==-1){
+        var newurl = new URL(window.location.href);
+        newurl.searchParams.set('q', newurl.searchParams.get('q')+' '+filter);
+        location.href=newurl.toString();
+    }
+    if(url.indexOf('-site')!=-1){
+        var search=document.getElementById('sb_form_q').value
+        document.getElementById('sb_form_q').value=search.replace(filter,'')
+        document.title=document.title.replace(filter,'')
+    }
+})();
