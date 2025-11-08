0b24c048d36cae992ce18b4088b28562dc66d8d9
zian <zian@zi.an>
2025-11-09 00:16:45 +0800

    update website

---
+++ js/offkab.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         offkab
+// @author       offkab
+// @include      */upload/*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.location.href=document.getElementsByTagName("img")[3].src
+    // Your code here...
+})();
